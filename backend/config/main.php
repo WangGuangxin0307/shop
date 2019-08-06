cf1e5f1faccbd7b17325c93b4ad7afd07fde5c92
it老王 <771155535@qq.com>
2019-08-06 20:28:13 +0800

    Signed-off-by: it老王 <771155535@qq.com>

--- backend/config/main.php
+++ backend/config/main.php
@@ -7,6 +7,7 @@ $params = array_merge(
 );
 return [
     'id' => 'app-backend',
+    'defaultRoute'=>'index',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
